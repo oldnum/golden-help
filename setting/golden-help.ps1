75ae6eaee691992af54b91c9c587d3fa781a0a1a
oldnum <tehno_droid@ukr.net>
2023-11-27 11:50:13 +0200

    upd

--- setting/golden-help.ps1
+++ setting/golden-help.ps1
@@ -44,7 +44,7 @@ function logos {
             Write-Host " |"
             Write-Host "[admin] start auto update"
             Start-Sleep -Seconds 2
-            Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", "irm https://raw.githubusercontent.com/oldnum/golden-help/main/setting/auto-install.ps1 | iex"
+            Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", "Start-Sleep -Seconds 1; Remove-Item '$([System.IO.Path]::Combine($env:USERPROFILE, 'Downloads'))\golden-help-main' -Recurse -Force;irm https://raw.githubusercontent.com/oldnum/golden-help/main/setting/auto-install.ps1 | iex; exit"
             exit
         }
     } else {
